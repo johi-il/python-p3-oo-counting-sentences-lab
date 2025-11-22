2f1553e4962e8d4264113ff8c0372b30aeef83ab
lewis wanjohi <i.johilewis@gmail.com>
2025-11-22 16:36:12 +0300

    done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+# count_sentences.py
+    def __init__(self, value=""):
+        self._value = ""
+        self.value = value  # use setter
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        if not self._value:
+            return 0
+        import re
+        # Split by sentence-ending punctuation (., ?, !)
+        sentences = re.split(r'[.!?]+', self._value)
+        # Remove empty strings caused by trailing punctuation
+        sentences = [s.strip() for s in sentences if s.strip()]
+        return len(sentences)
+
